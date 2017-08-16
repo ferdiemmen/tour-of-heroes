25cbe70371e106ea485b385a6949eb00c4ab85a0
ferdiemmen <fjemmen@gmail.com>
2017-08-16 09:12:01 +0200

    Fix styling bugs

--- src/app/components/media/media-align.component.ts
+++ src/app/components/media/media-align.component.ts
@@ -5,7 +5,8 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-media-align',
-  templateUrl: './media-align.component.html'
+  templateUrl: './media-align.component.html',
+  styleUrls: ['./media-align.component.scss'],
 })
 export class MediaAlignComponent {
   public position: string = 'center';
